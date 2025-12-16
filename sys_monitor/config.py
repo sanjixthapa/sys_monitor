15d2852b640d83d6a61917a0bbb8eee592e0e63e
Sanji <147115096+sanjixthapa@users.noreply.github.com>
2025-12-16 09:32:36 -0500

    Update system thresholds and email settings

--- sys_monitor/config.py
+++ sys_monitor/config.py
@@ -1,17 +1,17 @@
 # config.py
 
 # System Thresholds (Percentages)
-CPU_THRESHOLD = 1
-MEMORY_THRESHOLD = 1
-DISK_THRESHOLD = 1
+CPU_THRESHOLD = 70
+MEMORY_THRESHOLD = 70
+DISK_THRESHOLD = 70
 
 # Email Settings (Use an App Password for Gmail)
 EMAIL_ENABLED = True  # Set to True to enable
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
-SENDER_EMAIL = "sthapach@oswego.edu"
-SENDER_PASSWORD = "tznn wqco oqzh ioji"
-RECEIVER_EMAIL = "sanjeevanthapa22@gmail.com"
+SENDER_EMAIL = "sender@gmail.com"
+SENDER_PASSWORD = "senderpassword"
+RECEIVER_EMAIL = "reciever@gmail.com"
 
 # Database
 DB_NAME = "system_stats.db"
